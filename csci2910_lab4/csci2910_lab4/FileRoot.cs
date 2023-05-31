2ef7107a1d1764e490747c9da8625a5f27e04526
TMAWilliams <tmawilliams73@gmail.com>
2023-05-31 15:27:49 -0400

    Set up starting file structure

--- csci2910_lab4/csci2910_lab4/FileRoot.cs
+++ csci2910_lab4/csci2910_lab4/FileRoot.cs
@@ -1,6 +1,6 @@
 ﻿/**       
  *--------------------------------------------------------------------
- * 	   File name: Program.cs
+ * 	   File name: FileRoot.cs
  * 	Project name: csci2910_lab4
  *--------------------------------------------------------------------
  * Author’s name and email:	 Tessa Williams williamstm5@etsu.edu				
@@ -8,13 +8,17 @@
  *           Creation Date:	 05/31/2023		
  * -------------------------------------------------------------------
  */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
 namespace csci2910_lab4
 {
-    public class Program
+    public class FileRoot
     {
-        static void Main()
-        {
-            
-        }
+
     }
 }
